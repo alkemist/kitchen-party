842ef1695c747a23ee7aaa20126692be129834b6
Jérémy ACHAIN <achain.jeremy@gmail.com>
2022-01-20 17:15:03 +0100

    Test deploy env variable access

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -12,8 +12,15 @@ const targetPath = isProduction
 // in the process.env object thanks to dotenv
 const environmentFileContent = `
 export const environment = {
-   production: ${isProduction},
-   APP_NAME: "${process.env['APP_NAME']}",
+  production: ${isProduction},
+  APP_NAME: "${process.env['APP_NAME']}",
+  FIREBASE_API_KEY: "${process.env['FIREBASE_API_KEY']}",
+  FIREBASE_AUTH_DOMAIN: "${process.env['FIREBASE_AUTH_DOMAIN']}",
+  FIREBASE_PROJECT_ID: "${process.env['FIREBASE_PROJECT_ID']}",
+  FIREBASE_STORAGE_BUCKET: "${process.env['FIREBASE_STORAGE_BUCKET']}",
+  FIREBASE_MESSAGING_SENDER_ID: "${process.env['FIREBASE_MESSAGING_SENDER_ID']}",
+  FIREBASE_APP_ID: "${process.env['FIREBASE_APP_ID']}",
+  FIREBASE_MEASUREMENT_ID: "${process.env['FIREBASE_MEASUREMENT_ID']}",
 };
 `;
 //
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +9,5 @@ export class AppComponent {
   title = 'kitchen-party';
 
   constructor() {
-    console.log(environment);
   }
 }
