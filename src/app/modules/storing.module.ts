95c2a77dbd99a2e4789a417df331d0354121a7bc
Jérémy ACHAIN <jeremy.achain.external@stormshield.eu>
2022-02-04 14:40:36 +0100

    Begin kitchenIngredient module

--- src/app/modules/storing.module.ts
+++ src/app/modules/storing.module.ts
@@ -6,13 +6,13 @@ import {NgxsModule} from '@ngxs/store';
 import {IngredientState} from '../store/ingredient.state';
 import {RecipeState} from '../store/recipe.state';
 import {UserState} from '../store/user.state';
-import {KitchenState} from "../store/kitchen.state";
+import {KitchenIngredientState} from "../store/kitchen.state";
 
 const states = [
   UserState,
   IngredientState,
   RecipeState,
-  KitchenState,
+  KitchenIngredientState,
 ];
 
 @NgModule({
