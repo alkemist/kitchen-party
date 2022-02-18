1884b4d1bd790c1bf2e166f20564078761fdc33e
Jérémy ACHAIN <jeremy.achain.external@stormshield.eu>
2022-02-18 15:20:00 +0100

    Begin TU

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,43 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 import { AppComponent } from './app.component';
+import { TranslatingModule } from './modules/translating.module';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  @Component({selector: 'app-header', template: ''})
+  class HeaderStubComponent {
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        TranslatingModule,
+        ToastModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderStubComponent,
       ],
+      providers: [
+        MessageService
+      ]
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
