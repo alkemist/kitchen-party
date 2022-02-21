45dbbaded7897e3132969248c852f1522e9e066a
Jérémy ACHAIN <jeremy.achain.external@stormshield.eu>
2022-02-21 14:35:18 +0100

    Fix deploy

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -5,23 +5,21 @@ require('dotenv').config();
 // read the command line arguments passed with yargs
 const environment = argv.environment;
 const isProduction = environment === 'prod';
-const targetPath = isProduction
-  ? `./src/environments/environment.prod.ts`
-  : `./src/environments/environment.ts`;
+const targetPath = `./src/environments/environment.ts`;
 // we have access to our environment variables
 // in the process.env object thanks to dotenv
 const environmentFileContent = `
 export const environment = {
-  production: ${isProduction},
-  APP_NAME: "${process.env['APP_NAME']}",
-  FIREBASE_API_KEY: "${process.env['FIREBASE_API_KEY']}",
-  FIREBASE_AUTH_DOMAIN: "${process.env['FIREBASE_AUTH_DOMAIN']}",
-  FIREBASE_PROJECT_ID: "${process.env['FIREBASE_PROJECT_ID']}",
-  FIREBASE_STORAGE_BUCKET: "${process.env['FIREBASE_STORAGE_BUCKET']}",
-  FIREBASE_MESSAGING_SENDER_ID: "${process.env['FIREBASE_MESSAGING_SENDER_ID']}",
-  FIREBASE_APP_ID: "${process.env['FIREBASE_APP_ID']}",
-  FIREBASE_MEASUREMENT_ID: "${process.env['FIREBASE_MEASUREMENT_ID']}",
-  GOOGLE_CLOUD_OPERATIONS_API_KEY: "${process.env['GOOGLE_CLOUD_OPERATIONS_API_KEY']}",
+  production: ${ isProduction },
+  APP_NAME: "${ process.env['APP_NAME'] }",
+  FIREBASE_API_KEY: "${ process.env['FIREBASE_API_KEY'] }",
+  FIREBASE_AUTH_DOMAIN: "${ process.env['FIREBASE_AUTH_DOMAIN'] }",
+  FIREBASE_PROJECT_ID: "${ process.env['FIREBASE_PROJECT_ID'] }",
+  FIREBASE_STORAGE_BUCKET: "${ process.env['FIREBASE_STORAGE_BUCKET'] }",
+  FIREBASE_MESSAGING_SENDER_ID: "${ process.env['FIREBASE_MESSAGING_SENDER_ID'] }",
+  FIREBASE_APP_ID: "${ process.env['FIREBASE_APP_ID'] }",
+  FIREBASE_MEASUREMENT_ID: "${ process.env['FIREBASE_MEASUREMENT_ID'] }",
+  GOOGLE_CLOUD_OPERATIONS_API_KEY: "${ process.env['GOOGLE_CLOUD_OPERATIONS_API_KEY'] }",
 };
 `;
 //
@@ -30,5 +28,5 @@ writeFile(targetPath, environmentFileContent, function (err: Error) {
   if (err) {
     console.log(err);
   }
-  console.log(`Wrote variables to ${targetPath}`);
+  console.log(`Wrote variables to ${ targetPath }`);
 });
