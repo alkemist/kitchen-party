d7bac9c414139b09fcfe0a99bb5bed6604a1f6d2
Jérémy ACHAIN <achain.jeremy@gmail.com>
2022-03-02 13:17:52 +0100

    Fix mock date get time

--- src/app/tools/generate-pushid.spec.ts
+++ src/app/tools/generate-pushid.spec.ts
@@ -4,6 +4,7 @@ import { dateMock } from '../mocks/date.mock';
 describe('generate-pushid', () => {
   it('generate-pushid', () => {
     jest.spyOn(Math, 'random').mockReturnValue(0.6);
+    jest.spyOn(dateMock, 'getTime').mockReturnValue(460462320000);
     expect(generatePushID(dateMock)).toEqual('-5gpgLa-aaaaaaaaaaaa');
     expect(generatePushID()).toBeDefined();
   });
--- src/app/tools/time.helper.spec.ts
+++ src/app/tools/time.helper.spec.ts
@@ -4,6 +4,8 @@ import { dateMock } from '../mocks/date.mock';
 describe('TimeHelper', () => {
   it('calcHoursAfter', () => {
     const dateNow = new Date('2022-02-22 22:22');
+    jest.spyOn(dateMock, 'getTime').mockReturnValue(460462320000);
+    jest.spyOn(dateNow, 'getTime').mockReturnValue(1645564920000);
     const hours = TimeHelper.calcHoursAfter(dateMock, dateNow);
     expect(hours).toEqual(329195);
   });
