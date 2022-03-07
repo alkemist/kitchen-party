f7637559671828b3570e55978688600b7f46c234
Jérémy ACHAIN <achain.jeremy@gmail.com>
2022-03-07 23:13:15 +0100

    Finish backup script

--- scripts/backup.ts
+++ scripts/backup.ts
@@ -1,8 +1,9 @@
 // @ts-ignore
-const {writeFile} = require('fs');
-const {Firestore} = require('@google-cloud/firestore');
+const { writeFile } = require('fs');
+//const { Firestore } = require('@google-cloud/firestore');
+const firebase = require("firebase/app");
+const firestore = require("firebase/firestore");
 
-// read environment variables from .env file
 require('dotenv').config();
 
 const today = new Date();
@@ -12,32 +13,50 @@ const yyyy = today.getFullYear();
 const todayStr = `${ yyyy }-${ mm }-${ dd }`;
 
 // @ts-ignore
-const targetPath = `./backups/backup-${ todayStr }.json`;
 
 // Create a new client
-const firestore = new Firestore();
+firebase.initializeApp({
+  apiKey: process.env['FIREBASE_API_KEY'],
+  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+  projectId: process.env['FIREBASE_PROJECT_ID'],
+  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+  appId: process.env['FIREBASE_APP_ID'],
+  measurementId: process.env['FIREBASE_MEASUREMENT_ID']
+});
 
-async function limitToLastQuery() {
-  const collectionReference = firestore.collection('recipes');
-  const recipeDocuments = await collectionReference
-    .orderBy('slug')
-    .limitToLast(2)
-    .get();
-  const recipeDocumentsData = recipeDocuments.docs.map((d: any) => d.data());
-  recipeDocumentsData.forEach((doc: any) => {
-    console.log(doc.name);
+const firestoreRef = firestore.getFirestore();
+
+async function select(collectionName: string): Promise<any[]> {
+  const reference = firestore.collection(firestoreRef, collectionName);
+  const query = firestore.query(reference, firestore.orderBy('slug'));
+  const querySnapshot = await firestore.getDocs(query);
+
+  const documents: any[] = [];
+  querySnapshot.forEach((docSnapshot: any) => {
+    documents.push(docSnapshot.data());
   });
+  return documents;
 }
 
-limitToLastQuery().then(r => () => {
-  console.log(r);
+async function backup(type: string) {
+  const documents = await select(type);
+  const targetPath = `./backups/backup-${ type }-${ todayStr }.json`;
 
-  writeFile(targetPath, '', (err: Error) => {
+  writeFile(targetPath, JSON.stringify(documents), (err: Error) => {
     if (err) {
       console.log(err);
     }
-    console.log(`Wrote backup to ${ targetPath }`);
+    console.log(`Wrote backup "${ type }" to ${ targetPath }`);
   });
-});
+}
+
+backup('recipe');
+backup('ingredient');
+
+
+
+
+
 
 
